298ae62a3486831e5cb7a996669bc19894a84545
Brian Yang <brianwsyang@berkeley.edu>
2019-08-03 19:21:08 -0700

    v.0803.19.1920: need to parse json into javascript

--- vscode_version/embed_map.js
+++ vscode_version/embed_map.js
@@ -1,11 +1,17 @@
+function main() {
+    initMap();
+}
+
 function initMap() {
     // The location of Uluru
     var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
+    var usa_ctr = {lat: 37.09, lng: -95.71};
+    // The map, centered at USA
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {zoom: 4, center: usa_ctr});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
 
 }
 
+main();
