293c1e2d148608812317df171d346ea8eaec26df
Susan Lin <susan.lin@berkeley.edu>
2019-08-06 12:13:39 -0700

    finish form

--- vscode_version/submitform.php
+++ vscode_version/submitform.php
@@ -1,16 +1,35 @@
-    <?php
-        if($_POST["submit_form"] == "submit") {
-            //not sure if the code below should go inside this
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>DED Discovery Project</title>
+</head>
+
+<body>
+   
+   <?php
+        if(isset($_POST["submit_form"])) {
+            
         }
+        $name = $_POST["Name"];
+        $email = $_POST["Email"];
 
-            $name = $_POST["Name"];
-            $email = $_POST["Email"];
-
-            $fs = fopen("current.csv", "a");
-            fwrite($fs, $name . ", " . $email . "\n");
-            fclose($fs);
-
-            header("Location: submitted.html");
-            exit;
+        $fs = fopen("current.csv", "a");
+        fwrite($fs, $name . ", " . $email . "\n");
+        fclose($fs);
+        echo "hello";
+        
+        header("Location: submitted.html");
+        exit;
         
     ?>
+
+<body>
+</html>
