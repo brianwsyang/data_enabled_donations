1d6daf7d8cecb27b857351a7dec1b97339b57fac
Susan Lin <susan.lin@berkeley.edu>
2019-10-30 17:39:56 -0700

    attempting to use express

--- vscode_version/app.js
+++ vscode_version/app.js
@@ -2,4 +2,9 @@ var express = require("express");
  
 var app = express();
 
-app.use('/', express.static(__dirname + '/index.html'));
+app.use(express.static(__dirname + '/index.html'));
+
+var server = app.listen(3000, function(){
+    var port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
